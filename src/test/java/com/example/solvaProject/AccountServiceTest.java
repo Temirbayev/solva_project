dde768e788fd6c469afbf185e3b267ea982ba6c5
Temirbayev Temirlan <temirbayev002@gmail.com>
2023-09-28 02:58:43 +0600

    3. Написал тест AccountServiceTest исправленный

--- src/test/java/com/example/solvaProject/AccountServiceTest.java
+++ src/test/java/com/example/solvaProject/AccountServiceTest.java
@@ -1,56 +1,49 @@
 package com.example.solvaProject;
+
+import com.example.solvaProject.model.AccountEntity;
 import com.example.solvaProject.model.CurrencyEntity;
 import com.example.solvaProject.model.dto.AccountDto;
 import com.example.solvaProject.repository.AccountRep;
 import com.example.solvaProject.repository.CurrencyRep;
 import com.example.solvaProject.service.AccountService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.mockito.Mockito.*;
-
 @SpringBootTest
-@AutoConfigureMockMvc
 public class AccountServiceTest {
-
     @InjectMocks
     private AccountService accountService;
 
     @Mock
-    private AccountRep accountRep;
-
-    @Mock
     private CurrencyRep currencyRep;
 
-    @BeforeEach
-    public void setUp() {
-        MockitoAnnotations.initMocks(this);
-    }
+    @Mock
+    private AccountRep accountRep;
+
 
     @Test
-    public void testCreateAccountWithZeroLimit() {
-        // Создаем фиктивные данные для DTO с нулевым лимитом
+    public void testCreateAccountWithCurrencyConversion() {
+        // Создаем фиктивные данные для DTO
         AccountDto accountDto = new AccountDto();
         accountDto.setAccountNumber("123456");
         accountDto.setBalanceUsd(1000.0);
         accountDto.setLimitUsd(0.0);
 
-        // Создаем фиктивные данные для валюты
+        // Создаем фиктивные данные для валют
         CurrencyEntity currencyKzt = new CurrencyEntity();
         currencyKzt.setFromCurrency("USD");
         currencyKzt.setToCurrency("KZT");
-        currencyKzt.setExchangeRate(String.valueOf(420.0));
+        currencyKzt.setExchangeRate("420.0");
 
         CurrencyEntity currencyRub = new CurrencyEntity();
         currencyRub.setFromCurrency("USD");
         currencyRub.setToCurrency("JPY");
-        currencyRub.setExchangeRate(String.valueOf(110.0));
+        currencyRub.setExchangeRate("110.0");
 
+        // Мокируем поведение currencyRep
         when(currencyRep.findFirstByFromCurrencyAndToCurrency("USD", "KZT")).thenReturn(currencyKzt);
         when(currencyRep.findFirstByFromCurrencyAndToCurrency("USD", "JPY")).thenReturn(currencyRub);
 
@@ -58,13 +51,14 @@ public class AccountServiceTest {
         accountService.createAccount(accountDto);
 
         // Проверяем, что метод save был вызван с правильными данными
-        verify(accountRep, times(1)).save(argThat(accountEntity -> {
-            // Проверяем, что данные в AccountEntity правильно установлены, включая лимит 1000.0
-            return accountEntity.getAccountNumber().equals("123456")
-                    && accountEntity.getBalanceUsd() == 1000.0
-                    && accountEntity.getLimitUsd() == 1000.0
-                    && accountEntity.getBalanceKzt() == 420000.0 // 1000 USD * 420 KZT/USD
-                    && accountEntity.getBalanceRub() == 110000.0; // 1000 USD * 110 JPY/USD
-        }));
+        AccountEntity expectedAccountEntity = new AccountEntity();
+        expectedAccountEntity.setAccountNumber("123456");
+        expectedAccountEntity.setBalanceUsd(1000.0);
+        expectedAccountEntity.setLimitUsd(1000.0); // Потому что в limitUsd было 0
+        expectedAccountEntity.setBalanceKzt(420000.0); // 1000 USD * 420 KZT/USD
+        expectedAccountEntity.setBalanceRub(110000.0); // 1000 USD * 110 JPY/USD
+
+        // Проверяем, что метод save был вызван с ожидаемым объектом AccountEntity
+        verify(accountRep, times(1)).save(expectedAccountEntity);
     }
 }
